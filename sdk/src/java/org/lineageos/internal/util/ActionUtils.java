60423e595c4f8262093c867668f6908f6c37b72c
Pranav Vashi <neobuddy89@gmail.com>
2018-05-06 00:16:58 +0530

    Improve kill process code
    * Credits: DU.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- sdk/src/java/org/lineageos/internal/util/ActionUtils.java
+++ sdk/src/java/org/lineageos/internal/util/ActionUtils.java
@@ -4,6 +4,8 @@ import android.app.ActivityManager;
 import android.app.ActivityManagerNative;
 import android.app.ActivityOptions;
 import android.app.IActivityManager;
+import android.app.usage.UsageStats;
+import android.app.usage.UsageStatsManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -17,6 +19,8 @@ import android.widget.Toast;
 
 import org.lineageos.platform.internal.R;
 
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class ActionUtils {
@@ -32,51 +36,100 @@ public class ActionUtils {
      * @return {@code true} when a user application was found and closed.
      */
     public static boolean killForegroundApp(Context context, int userId) {
-        try {
-            return killForegroundAppInternal(context, userId);
-        } catch (RemoteException e) {
-            Log.e(TAG, "Could not kill foreground app");
-        }
-        return false;
+        return killForegroundAppInternal(context);
     }
 
-    private static boolean killForegroundAppInternal(Context context, int userId)
-            throws RemoteException {
-        try {
-            final Intent intent = new Intent(Intent.ACTION_MAIN);
-            String defaultHomePackage = "com.android.launcher";
-            intent.addCategory(Intent.CATEGORY_HOME);
-            final ResolveInfo res = context.getPackageManager().resolveActivity(intent, 0);
+    private static boolean killForegroundAppInternal(Context context) {
+        if (context.checkCallingOrSelfPermission(android.Manifest.permission.FORCE_STOP_PACKAGES) == PackageManager.PERMISSION_GRANTED) {
+            try {
+                PackageManager packageManager = context.getPackageManager();
+                final Intent intent = new Intent(Intent.ACTION_MAIN);
+                String defaultHomePackage = "com.android.launcher";
+                intent.addCategory(Intent.CATEGORY_HOME);
+                final ResolveInfo res = packageManager.resolveActivity(intent, 0);
+                if (res.activityInfo != null
+                        && !res.activityInfo.packageName.equals("android")) {
+                    defaultHomePackage = res.activityInfo.packageName;
+                }
 
-            if (res.activityInfo != null && !res.activityInfo.packageName.equals("android")) {
-                defaultHomePackage = res.activityInfo.packageName;
-            }
+                // Use UsageStats to determine foreground app
+                UsageStatsManager usageStatsManager = (UsageStatsManager)
+                    context.getSystemService(Context.USAGE_STATS_SERVICE);
+                long current = System.currentTimeMillis();
+                long past = current - (1000 * 60 * 60); // uses snapshot of usage over past 60 minutes
 
-            IActivityManager am = ActivityManagerNative.getDefault();
-            List<ActivityManager.RunningAppProcessInfo> apps = am.getRunningAppProcesses();
-            for (ActivityManager.RunningAppProcessInfo appInfo : apps) {
-                int uid = appInfo.uid;
-                // Make sure it's a foreground user application (not system,
-                // root, phone, etc.)
-                if (uid >= Process.FIRST_APPLICATION_UID && uid <= Process.LAST_APPLICATION_UID
-                        && appInfo.importance ==
-                        ActivityManager.RunningAppProcessInfo.IMPORTANCE_FOREGROUND) {
-                    if (appInfo.pkgList != null && (appInfo.pkgList.length > 0)) {
-                        for (String pkg : appInfo.pkgList) {
-                            if (!pkg.equals(SYSTEMUI_PACKAGE)
-                                    && !pkg.equals(defaultHomePackage)) {
-                                am.forceStopPackage(pkg, UserHandle.USER_CURRENT);
-                                return true;
-                            }
+                // Get the list, then sort it chronilogically so most recent usage is at start of list
+                List<UsageStats> recentApps = usageStatsManager.queryUsageStats(
+                    UsageStatsManager.INTERVAL_DAILY, past, current);
+                Collections.sort(recentApps, new Comparator<UsageStats>() {
+                    @Override
+                    public int compare(UsageStats lhs, UsageStats rhs) {
+                        long timeLHS = lhs.getLastTimeUsed();
+                        long timeRHS = rhs.getLastTimeUsed();
+                        if (timeLHS > timeRHS) {
+                            return -1;
+                        } else if (timeLHS < timeRHS) {
+                            return 1;
                         }
-                    } else {
-                        Process.killProcess(appInfo.pid);
-                        return true;
+                        return 0;
+                    }
+                });
+
+                IActivityManager iam = ActivityManagerNative.getDefault();
+                // this may not be needed due to !isLockTaskOn() in entry if
+                //if (am.getLockTaskModeState() != ActivityManager.LOCK_TASK_MODE_NONE) return;
+
+                // Look for most recent usagestat with lastevent == 1 and grab package name
+                // ...this seems to map to the UsageEvents.Event.MOVE_TO_FOREGROUND
+                String pkg = null;
+                for (int i = 0; i < recentApps.size(); i++) {
+                    UsageStats mostRecent = recentApps.get(i);
+                    if (mostRecent.mLastEvent == 1) {
+                        pkg = mostRecent.mPackageName;
+                        break;
                     }
                 }
+
+                if (pkg != null && !pkg.equals("com.android.systemui")
+                        && !pkg.equals(defaultHomePackage)) {
+
+                    // Restore home screen stack before killing the app
+                    Intent home = new Intent(Intent.ACTION_MAIN, null);
+                    home.addCategory(Intent.CATEGORY_HOME);
+                    home.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK
+                            | Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED);
+                    context.startActivity(home);
+
+                    // Kill the app
+                    iam.forceStopPackage(pkg, UserHandle.USER_CURRENT);
+
+                    // Remove killed app from Recents
+                    final ActivityManager am = (ActivityManager)
+                            context.getSystemService(Context.ACTIVITY_SERVICE);
+                    final List<ActivityManager.RecentTaskInfo> recentTasks =
+                            am.getRecentTasksForUser(ActivityManager.getMaxRecentTasksStatic(),
+                            ActivityManager.RECENT_IGNORE_HOME_AND_RECENTS_STACK_TASKS
+                                    | ActivityManager.RECENT_INGORE_PINNED_STACK_TASKS
+                                    | ActivityManager.RECENT_IGNORE_UNAVAILABLE
+                                    | ActivityManager.RECENT_INCLUDE_PROFILES,
+                                    UserHandle.CURRENT.getIdentifier());
+                    final int size = recentTasks.size();
+                    for (int i = 0; i < size; i++) {
+                        ActivityManager.RecentTaskInfo recentInfo = recentTasks.get(i);
+                        if (recentInfo.baseIntent.getComponent().getPackageName().equals(pkg)) {
+                            int taskid = recentInfo.persistentId;
+                            am.removeTask(taskid);
+                        }
+                    }
+                    return true;
+                } else {
+                    return false;
+                }
+            } catch (RemoteException remoteException) {
+                Log.d(TAG, "Caller cannot kill processes, aborting");
             }
-        } catch (RemoteException remoteException) {
-            // Do nothing; just let it go.
+        } else {
+            Log.d(TAG, "Caller cannot kill processes, aborting");
         }
         return false;
     }
