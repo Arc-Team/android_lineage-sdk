268a59f47a5518bd8d0ba0e857e626b9ce82ec5f
Pranav Vashi <neobuddy89@gmail.com>
2018-10-13 03:06:38 +0530

    sdk: Disable selinux warning alert for now
    * Do not trigger users for preliminary builds.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- sdk/src/java/lineageos/trust/TrustInterface.java
+++ sdk/src/java/lineageos/trust/TrustInterface.java
@@ -148,7 +148,7 @@ public class TrustInterface {
      *
      * @see #postNotificationForFeature
      */
-    public static final int TRUST_WARN_SELINUX = 1;
+    public static final int TRUST_WARN_SELINUX = 0;
 
     /**
      * Trust warning: Root access
